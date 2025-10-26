f0deccfa46ea9b7792a8b0ffddd45b46c6a0ee67
Rama <ramanaththakur1@gmail.com>
2025-10-26 19:34:43 +0530

    26/10/25 -- theming

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,17 @@
 import { LoginForm } from "@/components/login-form"
 import { requireUnauth } from "@/lib/auth-utils"
+import Image from "next/image";
+import Link from "next/link"
 
 export default async function Page() {
 
     await requireUnauth();
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
+            <div className="w-full max-w-sm flex flex-col gap-4">
+                <Link href="/" className="flex items-center gap-2 self-center font-medium">
+                    <Image src="/logos/KairoLogo.png" width={52} height={52} alt="KairoLogo" />
+                </Link>
                 <LoginForm />
             </div>
         </div>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,7 @@
 import { RegisterForm } from "@/components/register-form"
 import { requireUnauth } from "@/lib/auth-utils"
+import Image from "next/image"
+import Link from "next/link"
 
 
 const RegisterPage = async () => {
@@ -8,7 +10,10 @@ const RegisterPage = async () => {
 
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
+            <div className="w-full max-w-sm flex flex-col gap-4">
+                <Link href="/" className="flex items-center gap-2 self-center font-medium">
+                    <Image src="/logos/KairoLogo.png" width={52} height={52} alt="KairoLogo" />
+                </Link>
                 <RegisterForm />
             </div>
         </div>
