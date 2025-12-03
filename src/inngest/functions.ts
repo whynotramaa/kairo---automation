eebd43e6b26fdae7a3b51a14fbe4b03af8c8483c
Rama <ramanaththakur1@gmail.com>
2025-12-03 12:54:31 +0545

    03/12/25 --aiProvider gemini

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+
+export const execute = inngest.createFunction(
+    { id: "execute-ai" },
+    { event: "execute/ai" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "7s");
-        return { message: `Hello ${event.data.email}!` };
+        const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
+            model: google('gemini-2.5-flash'),
+            system: "You are Donald Trump",
+            prompt: "What is US Dream ? "
+        })
+
+        return steps;
     },
 );
