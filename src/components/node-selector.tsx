b05bc865bac21ea51233bcb976073ba86fd5db40
Rama <ramanaththakur1@gmail.com>
2025-12-23 19:19:40 +0545

    23-12-2025 --stripeIntegeration_succeeded

--- src/components/node-selector.tsx
+++ src/components/node-selector.tsx
@@ -29,6 +29,12 @@ const triggerNodes: NodeTypeOption[] = [
         label: "Google Form",
         description: "Runs the flow when a google form is submitted.",
         icon: '/logos/googleform.svg'
+    },
+    {
+        type: NodeType.STRIPE_TRIGGER,
+        label: "Stripe",
+        description: "Runs the flow when a stripe event is captured.",
+        icon: '/logos/stripe.svg'
     }
 ]
 
